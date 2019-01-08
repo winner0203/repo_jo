e9cc4a37f42a1047fdb908b51d5654935048a64d
winner0203 <jwoojo@gmail.com>
2019-01-08 13:52:05 +0900

    1

---
+++ test3.py
@@ -0,0 +1,27 @@
+qsdsmfnasnfd,amsfm,asndf
+aden-MacBook-Pro:git kakao$
+Jaden-MacBook-Pro:git kakao$
+Jaden-MacBook-Pro:git kakao$ cd repo_jo/
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$ ls
+test1.txt	test2.txt
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$ gi add *
+-bash: gi: command not found
+Jaden-MacBook-Pro:repo_jo kakao$ git add *
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$
+Jaden-MacBook-Pro:repo_jo kakao$ git commit -m "12"
+[master ff5dd2f] 12
+qsdsmfnasnfd,amsfm,asndf,samndf.nsafn,samndf.nsafn
